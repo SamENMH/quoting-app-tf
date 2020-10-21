e050d65efb60c2e88d8cf3bd1ece4962c1c4d30f
Samuel Hill <samuel.hill@gmail.com>
2020-10-21 22:33:51 +0100

    Changing project id again

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 terraform {
  backend "gcs" {
-  bucket = "terraform-state-quoting-app-293201"
+  bucket = "terraform-state-quoting-app"
   prefix = "terraform/state"
  }
 }
 
 provider "google" {
- project = "quoting-app-293201"
+ project = "quoting-app"
  region = "europe-west2"
  zone = "europe-west2-b"
 }
